87d5a9b6d161b044b0cace3da6e7bc13e8fc7bac
sourav kumar <souravk326@gmail.com>
2022-09-20 19:30:19 +0530

    main

--- main.go
+++ main.go
@@ -93,83 +93,10 @@ func main() {
 
 	// fmt.Println(today.Format("01-02-2006 Monday 15:04:05"))
 
-	// PROGRAM ON POINTERS
-	// var pointer *int
-	// var variable int = 1000
-	// fmt.Println("Just create without any valuse and just variable name", pointer)
-	// // fmt.Println("Just create without any valuse and with * ", *pointer)
-	// // fmt.Println("Just create without any valuse and with & ", &pointer)
+	
 
-	// pointer = &variable
-	// fmt.Printf("it may show some garbage value %v \n", pointer)
-	// fmt.Printf("it will show the value in the variable * %v \n", *pointer)
-	// fmt.Printf("it will show the address of variable  & %v \n", &pointer)
+	
 
-	//POGRAM ON SLICES
-	// var list = []string{"keyboard", "mouse", "moniter", "CPU"}
+	
 
-	// fmt.Printf("The list is of %T\n", list)
-	// fmt.Println(append(list[:]))
-	// list = append(list, "cable", "powersuply")
-	// fmt.Println(list[:4])
-	// fmt.Println(len(list))
-	// fmt.Println(cap(list))
-
-	// datascores := make([]int, 6)
-
-	// datascores[0] = 234
-	// datascores[1] = 634
-	// datascores[2] = 434
-	// datascores[3] = 534
-	// datascores[4] = 734
-
-	// fmt.Println(datascores)
-	// datascores = append(datascores, 3444, 456, 4545)
-	// fmt.Println(datascores)
-	// fmt.Println(datascores[:2])
-	// fmt.Println(datascores[2:])
-
-	// sort.Ints(datascores)
-	// fmt.Println("Sorted data", datascores)
-	// //how to remove a value slices based on index
-
-	// var teckstack = []string{"c", "c++", "JAVA", "golang", "js"}
-	// fmt.Println(teckstack)
-
-	// var index int = 2                                             //we are using the index to delete the from the slices
-	// teckstack = append(teckstack[:index], teckstack[index+1:]...) // about this ... next time
-	// fmt.Println(teckstack)
-
-	// // PROGRAM ON MAPS
-
-	// lang := make(map[string]string)
-
-	// lang["js"] = "javascript"
-	// lang["py"] = "python"
-	// lang["go"] = "Golang"
-	// lang["ts"] = "typescript"
-
-	// fmt.Println(lang)
-
-	// //lang = append(lang, ["MB"]"mongoDB")
-
-	// delete(lang, "ts")
-	// fmt.Println(lang)
-
-	// for keys, value := range lang {
-	// 	fmt.Printf("The is %v and the Value is %v\n", keys, value)
-	// }
-
-	// STRUCTS  IN GOLANG
-
-	sonu := User("sonu", "sonuak47@gmail.com", true, 14)
-	fmt.Print(sonu)
-
-}
-
-type User struct {
-	Name 	string
-	Email 	string
-	Status 	bool
-	Age 	int
 }
