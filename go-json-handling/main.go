7eec76ad1ea73337e44d0cf69d1b1aaf75e4817d
sourav kumar <souravk326@gmail.com>
2022-10-06 18:32:36 +0530

    go json filehandling

--- go-json-handling/main.go
+++ go-json-handling/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+type course struct {
+	Name     string
+	Price    int
+	Platform string
+	Password string
+	Tags     []string
+}
+
 func main() {
 
 	fmt.Println("Welcome To Go Lang Json Handling")
