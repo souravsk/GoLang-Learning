9838f92a0c91a3b07a0a296428f9fcdbb86535ae
sourav kumar <souravk326@gmail.com>
2022-10-08 19:30:47 +0530

    decode json data

--- go-json-handling/main.go
+++ go-json-handling/main.go
@@ -17,7 +17,8 @@ func main() {
 
 	fmt.Println("Welcome To Go Lang Json Handling")
 
-	EncodeJson()
+	//EncodeJson()
+	DecodeJson()
 }
 
 func EncodeJson() {
@@ -36,5 +37,38 @@ func EncodeJson() {
 	}
 
 	fmt.Printf("%s\n", finalJson)
+
+}
+
+func DecodeJson() {
+
+	jsonData := []byte(`
+
+		{
+			"Coursename": "HTML BootCamp",
+			"Price": 1000,
+			"Website": "youtube.com/freecodecamp",
+			"tags": ["web-dev","basic"]
+		}
+	`)
+
+	var lcoCourses course
+
+	checkValid := json.Valid(jsonData)
+
+	if checkValid {
+		fmt.Println("JSON was valid")
+		json.Unmarshal(jsonData, &lcoCourses)
+		fmt.Printf("%#v\n", lcoCourses)
+	} else {
+		fmt.Println("NOT A Valid JSON")
+	}
+
+	//same case where you just want to add data to key value pair
+
+	var mydata map[string]interface{}
+	//we are using map because we need key value pair the string for key because it alwaye a string bur we don't know what kind of data can be in a value so we use interface method
+	json.Unmarshal(jsonData, &mydata)
+	fmt.Printf("%#v\n", mydata)
 
 }
