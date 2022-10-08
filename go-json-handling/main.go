ef1eb0949ad9fc39f3d7087b9ebb4ad9ae7a0379
sourav kumar <souravk326@gmail.com>
2022-10-08 19:02:09 +0530

    json encoding

--- go-json-handling/main.go
+++ go-json-handling/main.go
@@ -1,17 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type course struct {
-	Name     string
+	Name     string `json:"Coursename"`
 	Price    int
-	Platform string
-	Password string
-	Tags     []string
+	Platform string   `json:"Website"`
+	Password string   `json:"-"`
+	Tags     []string `json:"tags,omitempty"`
 }
 
 func main() {
 
 	fmt.Println("Welcome To Go Lang Json Handling")
+
+	EncodeJson()
+}
+
+func EncodeJson() {
+
+	lcoCourses := []course{
+		{"HTML BootCamp", 1000, "youtube.com/freecodecamp", "thikhai", []string{"web-dev", "basic"}},
+		{"CSS BootCamp", 10000, "youtube.com/freecodecamp", "achahai", []string{"style", "basic"}},
+		{"JS BootCamp", 100000, "youtube.com/freecodecamp", "kuchthiknahihai", []string{"backend", "basic"}},
+	}
+
+	//package this data as  json data
+
+	finalJson, err := json.MarshalIndent(lcoCourses, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s\n", finalJson)
 
 }
