449196481d534bdeed0f787949a17cb80851d93d
sourav kumar <souravk326@gmail.com>
2022-10-11 19:45:27 +0530

    go lang mod

--- go-mod/go.mod
+++ go-mod/go.mod
@@ -1,3 +1,5 @@
 module github.com/souravsk/go-pratice
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0
--- go-mod/main.go
+++ go-mod/main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Pritnln("Welcome to Go lang Modules")
+	fmt.Println("Welcome to Go lang Modules")
+	greeter()
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func greeter() {
+	fmt.Println("Hey there mod users")
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Message from the http respone writer</h1>"))
 }
