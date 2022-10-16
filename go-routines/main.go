64cac8c9706f62e9393bffa1cd780f8d1783d49f
sourav kumar <souravk326@gmail.com>
2022-10-16 16:15:10 +0530

    go waitgroup

--- go-routines/main.go
+++ go-routines/main.go
@@ -1,9 +1,14 @@
+//SO the basic need to of goroutines is to make the process faster. goroutines are mostly like theards To use goroutines we use "go" keyword on the method name but the problem is the main funtion don't wait for the goroutines to go and compelete the process and come back so we use "wait group" which help as to make the main funtion to wait until the goroutines gets compelited.
+
 package main
 
 import (
 	"fmt"
 	"net/http"
+	"sync"
 )
+
+var wg sync.WaitGroup //usely this are in pointers, sync is go package and within sync we have waitgroup
 
 func main() {
 	// go greeter("hello") //what we are doing here is create the thead
@@ -18,8 +23,11 @@ func main() {
 	}
 
 	for _, web := range websitelist {
-		getStatusCode(web)
+		go getStatusCode(web)
+		wg.Add(1)
 	}
+
+	wg.Wait() //This wait always is on the last of the main function, the work of the wait() is to make the main() to wait until it get the Done() signl
 }
 
 // func greeter(s string) {
@@ -29,7 +37,9 @@ func main() {
 // 	}
 // }
 
+//we run it like this it is going to take time because it going to the each website one by one which is time taking
 func getStatusCode(endpoint string) {
+	defer wg.Done() //this done() should we run atlast so we use the defer so that it executed at the last, the work of done() to give signal to the wait() that work have been down.
 	res, err := http.Get(endpoint)
 	if err != nil {
 		fmt.Println("Oops in endpoint ")
