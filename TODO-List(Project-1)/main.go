9a45df258142f3c2a2ce070d5a3d6b0f1a49124d
sourav kumar <souravk326@gmail.com>
2022-10-22 12:29:42 +0530

    TODO list

---
+++ TODO-List(Project-1)/main.go
@@ -0,0 +1,32 @@
+package main
+
+
+var ren *renderer.Render
+var db *mgo.Database
+
+const(
+	hostName string = "localhost:27017"
+	dbName string = "todo-list"
+	collectionName string = "todo"
+	port string = ":8080"
+)
+
+type (
+	todoModel struct {
+		ID bson.ObjectId `bson:"_id,omitempty"`
+		Title string `bson:"title"`
+		Completed bool `bson:"completed"`
+		CreatedAt time.Time `bson:createAt`
+	}
+
+	todo struct {
+		ID string = `json:"id"`
+		Title string = `json:"title"`
+		Completed bool = `json:"completed"`
+		CreatedAt time.Time = `json:"created_at"`
+	}
+)
+
+func init(){
+	ren rend
+}
